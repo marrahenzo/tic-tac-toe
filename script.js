21254803e65a9118ee743fd8de06d0f2708f0f72
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-05-19 20:01:29 -0300

    Add functions for ending game

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const GameBoard = (() => {
 
 const Game = (() => {
   let currentPlayer;
+  let gameOver = false;
 
-  const start = (startingPlayer) => {
+  const start = (startingPlayer, otherPlayer) => {
     currentPlayer = startingPlayer.id;
     const board = document.querySelector("#game-board");
     for (let i = 0; i < GameBoard.board.length; i++) {
@@ -16,9 +17,9 @@ const Game = (() => {
       cell.className = "board-cell";
       cell.dataset.position = i;
       cell.addEventListener("click", () => {
-        if (GameBoard.board[cell.dataset.position] == "") {
+        if (GameBoard.board[cell.dataset.position] == "" && !gameOver) {
           Game.fillCell(cell.dataset.position);
-          Game.nextTurn(currentPlayer);
+          Game.nextTurn(startingPlayer, otherPlayer);
         }
       });
       board.appendChild(cell);
@@ -32,18 +33,64 @@ const Game = (() => {
       currentPlayer;
   };
 
-  const nextTurn = (playingPlayer) => {
-    currentPlayer = playingPlayer == "X" ? "O" : "X";
+  const checkGameOver = () => {
+    let board = GameBoard.board;
+    if (
+      (board[0] == "X" && board[1] == "X" && board[2] == "X") ||
+      (board[3] == "X" && board[4] == "X" && board[5] == "X") ||
+      (board[6] == "X" && board[7] == "X" && board[8] == "X") ||
+      (board[0] == "X" && board[3] == "X" && board[6] == "X") ||
+      (board[1] == "X" && board[4] == "X" && board[7] == "X") ||
+      (board[2] == "X" && board[5] == "X" && board[8] == "X") ||
+      (board[0] == "X" && board[4] == "X" && board[8] == "X") ||
+      (board[6] == "X" && board[4] == "X" && board[2] == "X")
+    ) {
+      Game.endGame("X");
+    } else if (
+      (board[0] == "O" && board[1] == "O" && board[2] == "O") ||
+      (board[3] == "O" && board[4] == "O" && board[5] == "O") ||
+      (board[6] == "O" && board[7] == "O" && board[8] == "O") ||
+      (board[0] == "O" && board[3] == "O" && board[6] == "O") ||
+      (board[1] == "O" && board[4] == "O" && board[7] == "O") ||
+      (board[2] == "O" && board[5] == "O" && board[8] == "O") ||
+      (board[0] == "O" && board[4] == "O" && board[8] == "O") ||
+      (board[6] == "O" && board[4] == "O" && board[2] == "O")
+    ) {
+      Game.endGame("O");
+    } else if (startingPlayer.marks + otherPlayer.marks > 7) {
+      Game.endGame("tie");
+    }
   };
 
-  return { start, nextTurn, fillCell, nextTurn };
+  const nextTurn = (startingPlayer, otherPlayer) => {
+    currentPlayer = currentPlayer == "X" ? otherPlayer.id : startingPlayer.id;
+  };
+
+  const endGame = (result) => {
+    switch (result) {
+      case "X":
+        console.log("X WINS");
+        break;
+      case "O":
+        console.log("O WINS");
+        break;
+      case "tie":
+        console.log("TIE");
+        break;
+    }
+  };
+
+  return { start, nextTurn, fillCell, checkGameOver, nextTurn, endGame };
 })();
 
 const Player = (playerId) => {
   let id = playerId;
-  return { id };
+  let marks = [];
+  return { id, marks };
 };
 
 const playerX = Player("X");
 const playerO = Player("O");
-Game.start(playerX);
+//Change into Game.start(playerX, playerO) but argument order changes depending
+// on choice of first player
+Game.start(playerX, playerO);
